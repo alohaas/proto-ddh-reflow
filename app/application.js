9cb87b65bafae3f02eec077f0d2b059fe92984ae
alohaas <livhaas@gmail.com>
2015-11-16 12:09:31 -0500

    scroll switch

--- app/application.js
+++ app/application.js
@@ -1,16 +1,27 @@
 "use strict";
 
-var App = {
+var $ = require('./jquery');
 
-  items: [
-    { item: 'Eggs' },
-    { item: 'Queso' },
-    { item: 'Guac' },
-    { item: 'Pico' }
-  ],
+var App = {
+  getTop: function(element) {
+    var topValue = element.offset().top;
+    return topValue;
+  },
+
+  scrollSwitch: function(target, className, topValue) {
+    var windowOffset = $(window).scrollTop();
+
+    $(window).on('scroll', function(e) {
+      var scrollPos = $(this).scrollTop();
+      scrollPos > topValue ? target.addClass(className) : target.removeClass(className);
+    });
+  },
 
   init: function init() {
-    console.log('App initialized.');
+    var $nav = $('#proto-ddh').find('#proto-nav');
+    var navTop = this.getTop($nav);
+
+    this.scrollSwitch($nav, 'proto-nav--fixed', navTop);
   }
 };
 
