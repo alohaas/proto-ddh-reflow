9133d5e8807d5b2f7b1f21d54c70f2d34c06ed42
alohaas <livhaas@gmail.com>
2015-11-17 10:24:39 -0500

    toggle UI working

--- app/application.js
+++ app/application.js
@@ -17,11 +17,21 @@ var App = {
     });
   },
 
+  toggleCounter: function($el) {
+    $el.on('click', '.type-toggle-item', function() {
+      var $target = $(this).parent('.type-toggle-items'),
+          i = $(this).index();
+      return $target.attr('data-loc', i);
+    });
+  },
+
   init: function init() {
     var $nav = $('#proto-ddh').find('#proto-nav');
     var navTop = this.getTop($nav);
+    var $toggle = $('#ia-type-toggle');
 
     this.scrollSwitch($nav, 'proto-nav--fixed', navTop);
+    this.toggleCounter($toggle);
   }
 };
 
